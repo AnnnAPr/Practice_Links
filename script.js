51a345fdc1eb32d312a1891cdbab3039a02341f8
AnnnAPr <77750289+AnnnAPr@users.noreply.github.com>
2021-08-15 21:16:15 -1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+let url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
+let req = new XMLHttpRequest();
+
+let data;
+let values = []; 
+
+let yScale;
+let xScale;
+let xAxisScale;
+let yAxisScale;
+
+let width = 900;
+let height = 500;
+let padding = 40;
+
+let svg = d3.select('svg');
+
+let drawCanvas = () => {
+  svg.attr('width', width);
+  svg.attr('height', height);
+}
+
+let createScales = () => {
+  yScale = d3.scaleLinear()
+                  .domain([0, d3.max(values, (item) => {
+                    return item[1];
+                  })])
+                  .range([0, height - (2*padding)]);
+  xScale = d3.scaleLinear()
+                  .domain([0, values.length - 1])
+                  .range([padding, width - padding]);
+  let datesArray = values.map((item) => {
+    return new Date(item[0]);
+
+  })  
+
+  xAxisScale = d3.scaleTime()
+                .domain([d3.min(datesArray), d3.max(datesArray)])
+                .range([padding, width - padding]);
+
+  yAxisScale = d3.scaleLinear()
+                .domain([0, d3.max(values, (item) => {
+                  return item[1];
+                })])
+                .range([height - padding, padding]);
+};
+
+let drawBars = () => {
+  svg.selectAll('rect')
+    .data(values)
+    .enter()
+    .append('rect')
+    .attr('class', 'bar')
+    .attr('width', (width - (2*padding)) / values.length)
+};
+
+let createAxes = () => {
+  let xAxis = d3.axisBottom(xAxisScale);
+  let yAxis = d3.axisLeft(yAxisScale);
+
+  svg.append('g')
+    .call(xAxis)
+    .attr('id', 'x-axis')
+    .attr('transform', 'translate(0, ' + (height - padding) + ')')
+
+  svg.append('g')
+    .call(yAxis)
+    .attr('id', 'y-axis')
+    .attr('transform', 'translate(' + padding + ' ,0)')
+};
+
+req.open("GET", url, true);
+req.onload = () => {
+  data = JSON.parse(req.responseText);
+  values = data.data;
+  console.log(values);
+  drawCanvas();
+  createScales();
+  drawBars();
+  createAxes();
+};
+req.send();
